85685d0478c946ac045b4172e7a602f3c9599e34
github username <github email address>
2018-12-11 08:31:24 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from 'react'
+
+
+
+
+  export default class Keypad extends React.Component {
+
+    handleEvent(event){
+      console.log('Entering password...')
+    }
+
+    render(){
+      return(
+        <input onKeyUp={this.handleEvent} type="password" />
+      )
+    }
+
+  }
